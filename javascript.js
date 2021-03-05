3131b27a9a8e02db343acb6b1bd35bc2f1e342c6
echeverriaalex <aleex.naahuel@outlook.com>
2021-03-05 11:35:06 -0300

    experimento con javascript

---
+++ javascript.js
@@ -0,0 +1,15 @@
+//document.getElementById("mifoto").innerHTML = "iconos/"
+
+
+document.getElementById("biografia") = "Hola bebota";
+
+
+
+function mostrarSecreto(){
+
+    contenido = document.getElementById("secreto");
+
+    document.getElementById("secreto").innerHTML =  "trabajo en Souto autoservicio :)";
+}
+
+mostrarSecreto();
