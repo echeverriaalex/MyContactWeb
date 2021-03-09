a123f215451a68e6da9c0f891f854caf3fcbe67d
echeverriaalex <aleex.naahuel@outlook.com>
2021-03-09 11:34:19 -0300

    test

---
+++ Person.js
@@ -0,0 +1,16 @@
+class Person {
+
+    constructor(name, lastName, age){
+    
+        var name;
+        var lastName;
+        var age;
+    }
+}
+
+/*
+person = new Person();
+person.name = "Alex";
+person.lastName = "Echeverria";
+person.age = 23;
+*/
